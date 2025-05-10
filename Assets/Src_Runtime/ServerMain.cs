23419c92a05b2e810b758e638e6c7bb58c050034
Lazyihui <chengbaozi6@qq.com>
2025-05-10 17:29:45 +0800

    <feature>add: Potocoles协议

--- Assets/Src_Runtime/ServerMain.cs
+++ Assets/Src_Runtime/ServerMain.cs
@@ -2,63 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Telepathy;
+using Unity.VisualScripting;
+
+namespace ServerMain {
+
+    public class ServerMain : MonoBehaviour {
+
+        Server server;
+        int port = 7777;
+        int messageSize = 1024;
+        bool isTearDown = false;
+
+        void Start() {
+            Application.runInBackground = true; // 允许后台运行
+
+            server = new Server(messageSize);
+            server.Start(port);
+            Debug.Log("服务器启动成功: " + port);
 
 
-public class ServerMain : MonoBehaviour {
+            server.OnConnected += (connID, str) => {
+                Debug.Log("服务端链接 " + connID);
+            };
 
-    Server server;
-    int port = 7777;
-    int messageSize = 1024;
-    bool isTearDown = false;
+            server.OnData += (connID, data) => {
+                Debug.Log("服务端接收数据 " + connID + " " + System.Text.Encoding.UTF8.GetString(data));
+                server.Send(connID, data);
+            };
 
-    void Start() {
-        Application.runInBackground = true; // 允许后台运行
-
-        server = new Server(messageSize);
-        server.Start(port);
-        Debug.Log("服务器启动成功: " + port);
-
-
-        server.OnConnected += (connID, str) => {
-            Debug.Log("服务端链接 " + connID);
-        };
-
-        server.OnData += (connID, data) => {
-            Debug.Log("服务端接收数据 " + connID + " " + System.Text.Encoding.UTF8.GetString(data));
-            server.Send(connID, data);
-        };
-
-        server.OnDisconnected += (connID) => {
-            Debug.Log("服务端断开链接 " + connID);
-        };
+            server.OnDisconnected += (connID) => {
+                Debug.Log("服务端断开链接 " + connID);
+            };
 
 
-    }
-
-    void Update() {
-        if (server != null) {
-            server.Tick(100); // 每秒30帧
         }
 
+        void Update() {
+            if (server != null) {
+                server.Tick(100); // 每秒30帧
+            }
 
-    }
 
-    void OnDestroy() {
-        TearDown();
-    }
-
-    void OnApplicationQuit() {
-        TearDown();
-    }
-
-    void TearDown() {
-        if (isTearDown) {
-            return;
         }
 
-        isTearDown = true;
-        if (server != null) {
-            server.Stop();
+        void OnDestroy() {
+            TearDown();
+        }
+
+        void OnApplicationQuit() {
+            TearDown();
+        }
+
+        void TearDown() {
+            if (isTearDown) {
+                return;
+            }
+
+            isTearDown = true;
+            if (server != null) {
+                server.Stop();
+            }
         }
     }
 }
